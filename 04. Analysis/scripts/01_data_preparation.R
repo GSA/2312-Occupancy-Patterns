e5fdc0a8e6b81021819c34fab06a76a3d06861c1
Ben Jaques-Lelsie <ben.jaques-leslie@gsa.gov>
2023-04-12 15:28:29 -0500

    Update 01_data_preparation.R

--- 04. Analysis/scripts/01_data_preparation.R
+++ 04. Analysis/scripts/01_data_preparation.R
@@ -14,8 +14,11 @@ library(purrr)
 author <- "Ben Jaques-Leslie"
 project_number <- 2312
 project_name <- "Occupancy Patterns"
+google_drive_home <- "G:/Shared drives/MSG Projects/1.0 Real Estate Solutions (LQ1)/2312 Work Patterns"
 data_folder_1 <- "Occupancy Data Pull"
-unique_identifier_1 <- c("employee_name")
+unique_identifier_1 <- c("employee_name","date")
+data_folder_2 <- "Rosters"
+unique_identifier_2 <- c("snapshot_date","employee_e_mail_address")
 
 # Determine file name -----------------------------------------------------
 
@@ -32,8 +35,11 @@ rmarkdown::render(input = here::here("04. Analysis",
                     author = author,
                     project_number = project_number,
                     project_name = project_name,
+                    google_drive_home = google_drive_home,
                     data_folder_1 = data_folder_1,
-                    unique_identifier_1 = unique_identifier_1
+                    unique_identifier_1 = unique_identifier_1,
+                    data_folder_2 = data_folder_2,
+                    unique_identifier_2 = unique_identifier_2
                   ),
                   output_file =
                     here::here("04. Analysis",
