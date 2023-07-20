98063e6ce2083e1f3bfa8cc3e7d3fd30f670fa34
Ben Jaques-Leslie <ben.jaques-leslie@gsa.gov>
2023-07-20 15:55:29 -0500

    Update make_specs.R
    Added new specifications to remove between day correlations

--- 04. Analysis/scripts/make_specs.R
+++ 04. Analysis/scripts/make_specs.R
@@ -1,17 +1,18 @@
 specs <- 
   tibble(
-  in_scale_or_rank = c(rep("scale", 6), rep("rank", 6)),
-  in_working_or_observation_window = rep(c(rep("observation", 3), rep("working", 3)), 2),
+  in_scale_or_rank = c(rep("scale", 8), rep("rank", 8)),
+  in_working_or_observation_window = rep(c(rep("observation", 4), rep("working", 4)), 2),
   in_weekday_correlation = rep(
     c(
       "correlation telework",
       "correlation status",
-      "proportion pairs"
+      "proportion pairs",
+      "none"
     ),
     4
   ),
-  in_number_of_clusters_start = rep(params$in_number_of_clusters_start, 12),
-  in_number_of_clusters_end = rep(params$in_number_of_clusters_end, 12)
+  in_number_of_clusters_start = rep(params$in_number_of_clusters_start, 16),
+  in_number_of_clusters_end = rep(params$in_number_of_clusters_end, 16)
 ) %>% 
   unite(col = "in_specification",everything(),remove = FALSE) %>% 
   mutate(
